1a43a7bcc26716efeb8232d3279f6deb6fc882ef
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2022-01-31 11:28:57 +0000

    idk what change

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -25,4 +25,41 @@ public class Main
         }
         ginger.kill();
     }
+
+    // Tutorial java intro week 1
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        // false false
+        // true false
+        // true true
+        return (!weekday || vacation);
+    }
+
+    public int diff21(int n) {
+        // dif21(19); -> 2
+        // dif21(10); -> 11
+        // dif21(21); -> 0
+        if (n<=21) {
+            return (21-n);
+        }
+        else {
+            return (n-21)*2;
+        }
+        /*
+        Inline if statement
+        // If A, then B else C
+        return (n<=21) ? (21-n) : (n-21)*2;
+        */
+    }
+
+    public boolean parrotTrouble(boolean talking, int hour) {
+        //true, 6 -> true
+        // true, 7 -> false
+        // false, 6 -> false
+        return (talking==true && (hour<7 || hour>20));
+    }
+
+    public boolean makes10(int a, int b) {
+        // Return true if sum is 10 or any number a or b is 10
+        return (a+b==10 || a==10 || b==10);
+    }
 }
