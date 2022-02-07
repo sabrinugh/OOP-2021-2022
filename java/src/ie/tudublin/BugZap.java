a8d9277b865909641e260f3da2042558292f2176
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2022-02-07 11:20:58 +0000

    bugs app test

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+import processing.core.PApplet;
+public class BugZap extends PApplet{
+    public void settings() {
+        size(500,500);
+    }
+    
+    public void setup() {
+        //
+    }
+
+    public void draw() {
+        //
+    }
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -18,5 +18,6 @@ public class Main
         // Tara Misty
         // Tara Tara
         
+        new BugZap();
     }
 }
