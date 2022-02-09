d39422cead9f9d1c0377e4970b8c548c7bd04fa6
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2022-02-09 14:36:38 +0000

    lab2 attempt

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,6 +5,22 @@ public class BugZap extends PApplet{
         size(500,500);
     }
     
+    float playerX = 150;
+    float playerY = 150;
+    float playerWidth = 250;
+
+    /* Write a method called void void drawPlayer(float x, float y,
+     loat w) that draws the player character, centered around the 
+    parameters x, y. You can use the line method to do this. 
+    You can pass variables as parameters to this method and also things 
+    like x + 20, w * 0.5f etc. I made an extra variable in this method 
+    called h for the height and set it to be half the w parameter. 
+    Don't forget to set the stroke color!
+    */
+    public void drawPlayer (float x, float y, float w){
+        
+    }
+
     public void setup() {
         // 
     }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,7 +1,9 @@
 package ie.tudublin;
-
 public class Main
 {
+    public static bugZap(){
+        new BugZap();
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello world");
