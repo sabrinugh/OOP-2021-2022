f748f52f0d4c8fd8573f071da19b0233986143f8
Sabrina Boc <98357578+sabrinugh@users.noreply.github.com>
2022-03-28 11:11:39 +0100

    random add

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -4,21 +4,31 @@ import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
+import ddf.minim.analysis.FFT;
+import ddf.minim.analysis.WindowFunction;
 import processing.core.PApplet;
+
+/*
+
+*/
 
 public class Audio2 extends PApplet
 {
-    Minim minim;
-    AudioPlayer ap;
-    AudioInput ai;
-    AudioBuffer ab;
+    Minim minim; // Allows access to audio file library
+    AudioPlayer ap; // Audio player - plays audio from device, encapsulate audio in program
+    AudioInput ai; // Takes audio from microphone
+    AudioBuffer ab; // Mix of audio left and right channels and arrange into array list
 
+    // 16 bits = sameple size, or resolution
+    // 44, 600 = 
     int mode = 0;
 
     float[] lerpedBuffer;
     float y = 0;
     float smoothedY = 0;
     float smoothedAmplitude = 0;
+
+    FFT fft; // Fast fourier transfer
 
     public void keyPressed() {
 		if (key >= '0' && key <= '9') {
@@ -36,7 +46,7 @@ public class Audio2 extends PApplet
 
     public void settings()
     {
-        size(1024, 1000, P3D);
+        size(1024, 1000);
         //fullScreen(P3D, SPAN);
     }
 
@@ -44,13 +54,15 @@ public class Audio2 extends PApplet
     {
         minim = new Minim(this);
         // Uncomment this to use the microphone
-        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
-        // ab = ai.mix; 
-        ap = minim.loadFile("heroplanet.mp3", 1024);
-        ap.play();
-        ab = ap.mix;
-        colorMode(HSB);
+        ai = minim.getLineIn(Minim.MONO, width, 44100, 16); // 44100 = sameple rate, 16 = resolution
+        ab = ai.mix; 
+        // ap = minim.loadFile("heroplanet.mp3", 1024); // 1024 is the frame size 
+        // ap.play();
+        // ab = ap.mix;
+        colorMode(RGB);
 
+        fft = new FFT(1024, 44100); // timesize is the frame size (1024) and the sample size
+        
         y = height / 2;
         smoothedY = y;
 
@@ -61,114 +73,37 @@ public class Audio2 extends PApplet
 
     public void draw()
     {
-        //background(0);
-        float halfH = height / 2;
-        float average = 0;
-        float sum = 0;
-        off += 1;
-        // Calculate sum and average of the samples
-        // Also lerp each element of buffer;
-        for(int i = 0 ; i < ab.size() ; i ++)
-        {
-            sum += abs(ab.get(i));
-            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
-        }
-        average= sum / (float) ab.size();
-
-        smoothedAmplitude = lerp(smoothedAmplitude, average, 0.1f);
-        
-        float cx = width / 2;
-        float cy = height / 2;
-
-        switch (mode) {
-			case 0:
-                background(0);
-                for(int i = 0 ; i < ab.size() ; i ++)
-                {
-                    //float c = map(ab.get(i), -1, 1, 0, 255);
-                    float c = map(i, 0, ab.size(), 0, 255);
-                    stroke(c, 255, 255);
-                    float f = lerpedBuffer[i] * halfH * 4.0f;
-                    line(i, halfH + f, i, halfH - f);                    
-                }
-                break;
-        case 1:
-            background(0);
-            for(int i = 0 ; i < ab.size() ; i ++)
-            {
-                //float c = map(ab.get(i), -1, 1, 0, 255);
-                float c = map(i, 0, ab.size(), 0, 255);
-                stroke(c, 255, 255);
-                float f = lerpedBuffer[i] * halfH * 4.0f;
-                line(i, halfH + f, halfH - f, i);                    
-            }
-            break;
-        case 2:
-            {
-                    float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-                    background(0, 0, 0, 10);
-                    stroke(c, 255, 255);	
-                    float radius = map(smoothedAmplitude, 0, 0.1f, 50, 300);		
-                    int points = (int)map(mouseX, 0, 255, 3, 10);
-                    int sides = points * 2;
-                    float px = cx;
-                    float py = cy - radius; 
-                    for(int i = 0 ; i <= sides ; i ++)
-                    {
-                        float r = (i % 2 == 0) ? radius : radius / 2; 
-                        // float r = radius;
-                        float theta = map(i, 0, sides, 0, TWO_PI);
-                        float x = cx + sin(theta) * r;
-                        float y = cy - cos(theta) * r;
-                        
-                        //circle(x, y, 20);
-                        line(px, py, x, y);
-                        px = x;
-                        py = y;
-                    }
-            }
-        case 3:
-            background(0);
-            strokeWeight(2);
-            noFill();
-            float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
-            float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
-            stroke(c, 255, 255);
-            circle(cx, cy, r);
-        case 4:
-        
-            background(0);
-            strokeWeight(2);
-            for(int i = 0 ; i < ab.size() ; i +=10)
-            {
-                //float c = map(ab.get(i), -1, 1, 0, 255);
-                float cc = map(i, 0, ab.size(), 0, 255);
-                stroke(cc, 255, 255);
-                float f = lerpedBuffer[i] * halfH * 4.0f;
-                line(i, halfH + f, i, halfH - f);
-                fill(cc);
-                circle(i, halfH + f, 5);                    
-                circle(i, halfH - f, 5);                    
-            }
-            break;
-
-        }
-        
-
-
-        
-        // Other examples we made in the class
-        /*
+        background(0);
         stroke(255);
-        fill(100, 255, 255);        
+
+        float halfH = height/2;
+
+        for (int i=0; i<ab.size(); i++) {
+            line(i, halfH, i, halfH+ab.get(i) * halfH); // ab.get(i) adjusts to screen.
+        }
+
+        fft.window(FFT.HAMMING);
+        fft.forward(ab); // Audio buffer and 
         
-        circle(width / 2, halfH, lerpedA * 100);
+        stroke(0, 255, 40);
+        for (int i=0; i<fft.specSize(); i++) {
+            line(i, 0, i, fft.getBand(i) * 10); // 0 means top of screen
+        }
 
-        circle(100, y, 50);
-        y += random(-10, 10);
-        smoothedY = lerp(smoothedY, y, 0.1f);        
-        circle(200, smoothedY, 50);
-        */
+        int maxIndex = 0;
+        
+        
+        for (int i=0; i<fft.specSize(); i++) {
+            if (fft.getBand(i) > fft.getBand(maxIndex)) {
+                maxIndex = i;
+            }
+        }
 
+        float freq = fft.indexToFreq(maxIndex);
+        textSize(75);
+        fill(255);
+
+        text("Freq: "+ freq, 100, 200);
+        
     }        
 }
